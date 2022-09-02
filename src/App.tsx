e7e33df991561770d4d7a36bb3c7200c8a45b79a
Dzevad <dzevad.del@gmail.com>
2022-09-02 20:30:54 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import styles from './App.module.css';
+
+function App() {
+  return <div className='app'>hello world</div>;
+}
+
+export default App;
